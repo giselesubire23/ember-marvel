181e3d0d6484d641906e6ea905dd1d3799f4666b
Gisele Subire <gisele.subire@lello.com.br>
2016-11-16 11:37:26 -0200

    ação em form

--- app/routes/details.js
+++ app/routes/details.js
@@ -1,18 +1,22 @@
 import Ember from 'ember';
 
 
+
 export default Ember.Route.extend({
 
 	model : function(params) {
+
 		return $.getJSON("https://gateway.marvel.com:443/v1/public/characters/" + params.id + "?apikey=6bd0ee806fbb8d26a9575992adc6b98b").then(function(data){
 
-			console.log(data.data.results[0]);
+			//console.log(data.data.results[0]);
+			return data.data.results; 				
 
-			return data.data.results;  
-
-		});
-	},
-
-
+		});	
+		
+	}
 
 });
+
+
+
+ 
